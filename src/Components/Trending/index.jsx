83fb52524e24dde2562564d98bea495512990bf6
Vinay <vinay.shermauryan@gmail.com>
2022-06-01 23:29:38 +0530

    added trending page:
     Responsiveness pending

--- src/Components/Trending/index.jsx
+++ src/Components/Trending/index.jsx
@@ -1,22 +1,34 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "../../../node_modules/react-tabs/style/react-tabs.css";
 
-import "react-tabs/style/react-tabs.css";
 import { Box, Button, Grid, makeStyles, Typography } from "@material-ui/core";
 // import FlashOnIcon from "@mui/icons-material/FlashOn";
 import { TRENDING_CONFIGS } from "../../Configs/Trending";
+import { THEME_COLOR } from "../../Configs/Theme";
 
 const Trending = () => {
   const classes = useStyles();
 
   return (
     <main className={`${classes.containerTrending} containerTrending`}>
-      <Tabs>
+      <Tabs
+        id="controlled-tabs"
+        defaultFocus={true}
+        selectedTabClassName="underline"
+        className={`${classes.tabsClass} tabsClass`}
+      >
         <TabList>
           {Object.keys(TRENDING_CONFIGS).map((trending, index) => {
             return (
               <Tab key={`${trending}-${index}`}>
-                <Button Boxor="outline-primary">{trending}</Button>
+                <Button
+                  Boxor="outline-primary"
+                  className={`${classes.tabButton} tabButton`}
+                  id="tabButton"
+                >
+                  {trending}
+                </Button>
               </Tab>
             );
           })}
@@ -40,8 +52,18 @@ const Trending = () => {
                   </Grid>
                   <Grid item xs={12} sm={12} md={8} lg={8}>
                     <Box className="content">
-                      <Typography variant={"h2"}>{title}</Typography>
-                      <Typography variant="body">{subtitle}</Typography>
+                      <Typography
+                        variant={"h2"}
+                        className={`${classes.trendingTitle} trendingTitle`}
+                      >
+                        {title}
+                      </Typography>
+                      <Typography
+                        variant="body"
+                        className={`${classes.trendingSubTitle} trendingSubTitle`}
+                      >
+                        {subtitle}
+                      </Typography>
 
                       {link && (
                         <a
@@ -74,12 +96,50 @@ export default Trending;
 
 const useStyles = makeStyles((theme) => ({
   containerTrending: {
+    padding: "6% 4%",
+    backgroundColor: THEME_COLOR.light_sky,
+  },
+
+  tabsClass: {
     width: "60%",
     margin: "auto",
-    padding: "6% 4%",
   },
   tabMatter: {
-    padding: "5%",
+    padding: "4% 5%",
+    backgroundColor: THEME_COLOR.light_sky,
   },
-  tabContainer: {},
+  tabContainer: {
+    backgroundColor: "green",
+  },
+  tabButton: {
+    fontSize: "1.2rem",
+    fontWeight: " 600",
+    color: `${THEME_COLOR.main_color} !important`,
+
+    "&:hover": {
+      backgroundColor: `${THEME_COLOR.main_color} !important`,
+      color: "white !important",
+      boxShadow: "none",
+    },
+    "&:active": {
+      boxShadow: "none",
+      color: "white !important",
+      backgroundColor: `${THEME_COLOR.color_3} !important`,
+    },
+  },
+
+  trendingTitle: {
+    color: `${THEME_COLOR.main_color} !important`,
+    fontWeight: "800",
+    fontSize: "2.5rem !important",
+    marginBottom: "4%",
+  },
+
+  trendingSubTitle: {
+    color: `${THEME_COLOR.color_3} !important`,
+    // fontWeight: "500",
+    fontSize: "1.2rem",
+    fontWeight: "700",
+    lineHeight: "1.3",
+  },
 }));
---
+++ src/Configs/Theme/index.js
@@ -0,0 +1,5 @@
+export const THEME_COLOR = {
+  main_color: "#0D274D",
+  light_sky: "#F8FBFF",
+  color_3: "#006699",
+};
