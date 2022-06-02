6a8cea133fb8669d77ca00509e2c0e5dadb900b5
Vinay <vinay.shermauryan@gmail.com>
2022-06-02 23:33:31 +0530

    fix: Trending page done

--- src/Components/Trending/index.jsx
+++ src/Components/Trending/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "../../../node_modules/react-tabs/style/react-tabs.css";
+import "./style.css";
 
 import { Box, Button, Grid, makeStyles, Typography } from "@material-ui/core";
-// import FlashOnIcon from "@mui/icons-material/FlashOn";
+import { FlashOn as FlashOnIcon } from "@material-ui/icons";
 import { TRENDING_CONFIGS } from "../../Configs/Trending";
 import { THEME_COLOR } from "../../Configs/Theme";
 
@@ -12,22 +13,31 @@ const Trending = () => {
 
   return (
     <main className={`${classes.containerTrending} containerTrending`}>
+      <Box className={`${classes.trendingLogo} trendingLogo`}>
+        <FlashOnIcon className={`${classes.flashIcon} flashIcon`} />
+        <Typography
+          variant="h4"
+          className={`${classes.trendingText} trendingText`}
+        >
+          Trending
+        </Typography>
+      </Box>
+      <hr className={`${classes.hr} hr`} />
       <Tabs
         id="controlled-tabs"
         defaultFocus={true}
         selectedTabClassName="underline"
-        className={`${classes.tabsClass} tabsClass`}
       >
-        <TabList>
-          {Object.keys(TRENDING_CONFIGS).map((trending, index) => {
+        <TabList className={`${classes.tabsClass} tabsClass`}>
+          {Object.values(TRENDING_CONFIGS).map((trending, index) => {
             return (
-              <Tab key={`${trending}-${index}`}>
+              <Tab key={`${trending}-${index}`} style={{ padding: 0 }}>
                 <Button
                   Boxor="outline-primary"
                   className={`${classes.tabButton} tabButton`}
                   id="tabButton"
                 >
-                  {trending}
+                  {trending.label}
                 </Button>
               </Tab>
             );
@@ -42,15 +52,22 @@ const Trending = () => {
                 className={`${classes.tabContainer} tabContainer`}
               >
                 <Grid container className={`${classes.tabMatter} tabMatter`}>
-                  <Grid item xs={12} sm={12} md={4} lg={4}>
+                  <Grid
+                    item
+                    xs={12}
+                    sm={6}
+                    md={6}
+                    lg={4}
+                    className={`${classes.trendingImageGrid} trendingImageGrid`}
+                  >
                     <img
-                      className="img-fluid"
                       src={icon}
                       alt={title}
+                      className={`${classes.trendingImage} trendingImage`}
                       style={{ width: "250px", height: "250px" }}
                     />
                   </Grid>
-                  <Grid item xs={12} sm={12} md={8} lg={8}>
+                  <Grid item xs={12} sm={6} md={6} lg={8}>
                     <Box className="content">
                       <Typography
                         variant={"h2"}
@@ -102,19 +119,26 @@ const useStyles = makeStyles((theme) => ({
 
   tabsClass: {
     width: "60%",
-    margin: "auto",
+    margin: "10px auto",
+    borderBottom: `5px solid ${THEME_COLOR.main_color} !important`,
+    "&:active": {
+      boxShadow: "none",
+      color: `${THEME_COLOR.light_sky} !important`,
+      backgroundColor: THEME_COLOR.light_sky,
+    },
   },
+
   tabMatter: {
     padding: "4% 5%",
     backgroundColor: THEME_COLOR.light_sky,
   },
-  tabContainer: {
-    backgroundColor: "green",
-  },
+  tabContainer: { width: "60%", margin: "auto" },
   tabButton: {
-    fontSize: "1.2rem",
-    fontWeight: " 600",
+    fontSize: "1rem",
+    fontWeight: "700",
     color: `${THEME_COLOR.main_color} !important`,
+    minWidth: "180px",
+    width: "100%",
 
     "&:hover": {
       backgroundColor: `${THEME_COLOR.main_color} !important`,
@@ -123,15 +147,17 @@ const useStyles = makeStyles((theme) => ({
     },
     "&:active": {
       boxShadow: "none",
-      color: "white !important",
+      // color: `${THEME_COLOR.light_sky} !important`,
+      color: "red !important",
       backgroundColor: `${THEME_COLOR.color_3} !important`,
+      borderBottom: "6px solid blue !important",
     },
   },
 
   trendingTitle: {
     color: `${THEME_COLOR.main_color} !important`,
     fontWeight: "800",
-    fontSize: "2.5rem !important",
+    fontSize: "2.5rem",
     marginBottom: "4%",
   },
 
@@ -141,5 +167,19 @@ const useStyles = makeStyles((theme) => ({
     fontSize: "1.2rem",
     fontWeight: "700",
     lineHeight: "1.3",
+  },
+
+  flashIcon: {
+    fontSize: "60px",
+    transform: "rotate(15deg)",
+    margin: "0 10px 0 0",
+    color: "#0d274d",
+  },
+  trendingLogo: {
+    display: "flex",
+    flexDirection: "row",
+    margin: "0 0 0 45%",
+    fontWeight: "800",
+    color: "#0d274d",
   },
 }));
