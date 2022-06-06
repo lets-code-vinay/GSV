eaa721ec96aad5a9f874f3bb0225e88d458b1281
Vinay <vinay.shermauryan@gmail.com>
2022-06-06 21:12:39 +0530

    carousel desktop fixed

--- src/Views/Homepage/index.jsx
+++ src/Views/Homepage/index.jsx
@@ -5,7 +5,7 @@ import Trending from "../../Components/Trending";
 import OurClients from "../../Components/OurClient/OurClients";
 import Footer from "../../Components/Footer";
 import InfinitySliderContainer from "../../Components/InfinitySliderContainers";
-import MainSlider from "../../Components/MainSlider/indexc";
+import MainSlider from "../../Components/MainSlider";
 
 const Homepage = () => {
   return (
